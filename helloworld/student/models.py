9ebe3a590d8af2fc1c1dc444314524f96bf884c1
Apoorva Joshi <xunl3kah@gmail.com>
2017-03-18 01:04:27 +0530

    Update models.py

--- helloworld/student/models.py
+++ helloworld/student/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class student(models.Model):
 	name = models.CharField(max_length=40)
-	email = models.CharField(max_length=50)
+	email = models.EmailField(max_length=50)
 	workingunder = models.CharField(max_length=40)
 	searchingon = models.CharField(max_length=500)
 	
