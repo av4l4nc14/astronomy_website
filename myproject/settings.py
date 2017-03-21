b14c25747f95eb3d2eaf332146191cac87786031
Bandaru Harsha Vardhan <bandaru.harshavardhan17@gmail.com>
2017-03-22 00:15:41 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'faculty.apps.FacultyConfig',
-    'student',	
+    'student.apps.StudentConfig',	
 ]
 
 MIDDLEWARE = [
