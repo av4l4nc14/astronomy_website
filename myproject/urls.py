c33e2a9ee32f7998397abee24af70191599edd07
Bandaru Harsha Vardhan <bandaru.harshavardhan17@gmail.com>
2017-04-11 14:29:12 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^student/',include('student.urls'),name='student'),
     url(r'^faculty/',include('faculty.urls'),name='faculty'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
