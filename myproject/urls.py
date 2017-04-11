88d88c7f5e91e315b6294f8a8820fd6510258251
Bandaru Harsha Vardhan <bandaru.harshavardhan17@gmail.com>
2017-04-11 14:26:37 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^student/',include('student.urls')),
-    url(r'^faculty/',include('faculty.urls')),
+    url(r'^student/',include('student.urls'),name='student'),
+    url(r'^faculty/',include('faculty.urls'),name='faculty'),
 ]
