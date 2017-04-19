81a7ddcd5d325d7d8a916af92ab122018145cd07
Apoorva Joshi <xunl3kah@gmail.com>
2017-04-19 10:28:19 +0530

    Delete urls.py

--- helloworld/faculty/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.views.generic import TemplateView
-from . import views
-
-urlpatterns=[
-	url(r'^$',views.index, name = 'index'),
-	url(r'^About$',TemplateView.as_view(template_name="fc.html"),
-
-]
